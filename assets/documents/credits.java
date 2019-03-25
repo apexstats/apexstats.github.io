0c22e5a4fa1c4f61c212ee04a32b0171d7ca463b
apexstats <48800181+apexstats@users.noreply.github.com>
2019-03-25 14:57:26 +0200

    Create credits.java

---
+++ assets/documents/credits.java
@@ -0,0 +1,10 @@
+public class Documents {
+ /* Just trying to fit a lot of languages to this repo
+ * Website Creator: RedstonedLife
+ * Bot Creator: Takitsu21
+ * Bot Contributor: RedstonedLife
+ *\
+ 
+ 
+ 
+}
